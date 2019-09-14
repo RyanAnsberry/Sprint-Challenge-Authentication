40f24584658473826bac26478e157ef0b5248e91
Ryan Ansberry <ansman92@gmail.com>
2019-09-13 18:20:59 -0700

    Created 2 test for each endpoint

--- users-model.js
+++ users-model.js
@@ -2,18 +2,8 @@ const db = require('./database/dbConfig.js');
 
 module.exports = {
   add,
-  find,
-  findBy,
   findById
 };
-
-function find() {
-  return db('users').select('id', 'username', 'password');
-}
-
-function findBy(filter) {
-  return db('users').where(filter);
-}
 
 function add(user) {
   return db('users')
